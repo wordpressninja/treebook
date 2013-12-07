87aef8dbfb145c75393592fd8651aa0048108cd1
root <root@wordpressninja.(none)>
2013-12-07 07:55:01 +0000

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'c29913b4d1b4afaebcab0570a5fd0fe0e73c59512323e8f068df3f676d20e1e8b22d1fc70c15c8f166bcf1795bf483d66a447736490c3bb217ce809adaba3f4f'
